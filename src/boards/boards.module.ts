dc9ff8811f4e4456294d190fe71b1e8efb4ae05f
minjin.ki <minjin.ki@navercorp.com>
2024-05-17 13:10:05 +0900

    test

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from "@nestjs/common";
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { TestModule } from "src/test/test.module";
 
 @Module({
-  imports: [TestModule],
+  imports: [forwardRef(() => TestModule)],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
--- src/test/test.module.ts
+++ src/test/test.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from "@nestjs/common";
 import { BoardsModule } from 'src/boards/boards.module';
 import { TestService } from './test.service';
 import { TestController } from './test.controller';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [forwardRef(() => BoardsModule)],
   providers: [TestService],
   controllers: [TestController]
 })
